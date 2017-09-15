8f1aa624605bd81dac6444274a5deb491decf5b2
Yuferev Kirill <yuferev.k@protonmail.ch>
2017-09-15 11:03:09 +0300

    another try

--- bash/pickhostname.sh
+++ bash/pickhostname.sh
@@ -9,14 +9,18 @@
 # ==================
 # === DEBUG MODE ===
 # ==================
-#  Uncomment next line if you want to turn debug mode ON
+#  Uncomment next line if you want to turn debug mode ON.
 #set -x
 
 # =========================
 # === STATIC PARAMETERS ===
 # =========================
+#  You'll need to modify next parameters if your
+#+ files will be located somewhere else and/or
+#+ have another name.
 AVAIL_HOSTNAMES="available_hostnames"
 INUSE_HOSTNAMES="inuse_hostnames"
+
 
 # =================
 # === FUNCTIONS ===
@@ -31,68 +35,59 @@ usage(){
 #+ is it available to read by current user and is it empty. Then it
 #+ checks if $INUSE_HOSTNAMES is present
 check_file(){
-	if [[ -f $AVAIL_HOSTNAMES ]]; then
-		if [[ -r $AVAIL_HOSTNAMES ]]; then
-			if [[ -s $AVAIL_HOSTNAMES ]]; then
-				echo "INFO: $AVAIL_HOSTNAMES file is present, checking $INUSE_HOSTNAME..."
-				if [[ -f $INUSE_HOSTNAMES ]]; then
-					if [[ -w $INUSE_HOSTNAMES ]]; then
-						echo "INFO: Everything seems to be OK for now. Continuing..."
-					else
-						echo "ERROR: unable to write to $INUSE_HOSTNAMES. Does it have correct permissions?"
-					fi
-				else
-					touch $INUSE_HOSTNAMES
-					if [[ -f $INUSE_HOSTNAME ]]; then
-						echo "INFO: Everything seems to be OK for now. Continuing..."
-					else
-						echo "ERROR: unable to create $INUSE_HOSTNAMES. Do you have correct access rights?"
-						exit 1
-					fi
-				fi
-			else
-				echo "ERROR: file $AVAIL_HOSTNAMES is empty. Unable to proceed. Did you run out of hostnames?"
-				exit 1
-			fi
-		else
-			echo "ERROR: unable to read contents of $AVAIL_HOSTNAMES. Does it have correct permissions?"
-			exit 1
-		fi
+	if [[ -f $AVAIL_HOSTNAMES ]] && [[ -r $AVAIL_HOSTNAMES ]] && [[ -s $AVAIL_HOSTNAMES ]]; then
+	if test -f $AVAIL_HOSTNAMES && test -r $AVAIL_HOSTNAMES && test -s $AVAIL_HOSTNAMES; then
+		echo "INFO: $AVAIL_HOSTNAMES file is present, checking $INUSE_HOSTNAME..."
 	else
-		echo "ERROR: file $AVAIL_HOSTNAMES doesn't exist. Did you configure the script properly?"
+		echo "ERROR: something wrong with $AVAIL_HOSTNAMES file."
+		echo "It is either not present or non-writable or empty. Unable to proceed."
+		exit 1
+	fi
+	if [[ -f $INUSE_HOSTNAMES ]] && [[ -w $INUSE_HOSTNAMES ]]; then
+		echo "INFO: everything seems to be OK for now. Continuing..."
+	else
+		echo "ERROR: something is wrong with $INUSE_HOSTNAMES file."
+		echo "It is either not present or non-writable. Unable to proceed."
 		exit 1
 	fi
 }
 
-#  Function pick_and_cut picks desired number of hostnames
+#  Function pick picks desired number of hostnames
 #+ from $AVAIL_HOSTNAMES file and then puts them into
 #+ $INUSE_HOSTNAMES. Then result is shown to a user.
-pick_and_cut(){
+pick(){
 	HOSTNAME=`shuf -n $NUMBER $AVAIL_HOSTNAMES`
 	sed -i -e '/$HOSTNAME/{w $INUSE_HOSTNAMES' -e 'd}' $AVAIL_HOSTNAMES
 	echo "$HOSTNAME"
 }
 
-detect_command(){
-	if [[ -n "$1" ]]; then # check if 1st argument is present
-		case $1 in
-			"pick") # case 1st argument is a "pick" command
-					if [ -n "$2" ] ; then # check if 2nd argument is present
-						if [[ "$2" =~ ^[-+]?[0-9]+$ ]] ; then # check if 2nd argument is a number
+reset(){
+	cat $INUSE_HOSTNAMES >> $AVAIL_HOSTNAMES
+	> $INUSE_HOSTNAMES
+}
+
+# ===================
+# === MAIN SCRIPT ===
+# ===================
+check_file
+if [[ -n "$1" ]]; then # check if 1st argument is present
+	case $1 in
+		"pick") # case 1st argument is a "pick" command
+				if [[ -n "$2" ]]; then # check if 2nd argument is present
+					if [[ "$2" =~ ^[-+]?[0-9]+$ ]]; then # check if 2nd argument is a number
 							NUMBER=`echo $2 | sed -e "s/[^[:digit:]]//g"`
-							pick_and_cut
+							pick
 						else # if 2nd argument isn't a number
 							NUMBER=1
-							pick_and_cut
+							pick
 						fi
 					else # if 2nd argument isn't present
 						NUMBER=1
-						pick_and_cut
+						pick
 					fi
 					;;
 			"reset") # and it is "reset" command
-					cat $INUSE_HOSTNAMES >> $AVAIL_HOSTNAMES
-					> $INUSE_HOSTNAMES
+					reset
 					;;
 			*)
 					echo "ERROR: unknown command. Unable to proceed."
@@ -102,13 +97,4 @@ detect_command(){
 	else # if no arguments are present
 		usage
 		exit 1
-	fi
-}
-
-# ===================
-# === MAIN SCRIPT ===
-# ===================
-check_file
-ARGS=( $@ )
-
-#detect_command
+fi
